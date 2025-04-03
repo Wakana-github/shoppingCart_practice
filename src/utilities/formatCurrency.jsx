a2acffbdd841548885568f2a22140cbb2b411bad
wakana-github <wakana_devwork@proton.me>
2025-04-03 23:13:44 +1100

    Deploy dist folder to gh-pages

--- src/utilities/formatCurrency.jsx
+++
@@ -1,8 +0,0 @@
-const CURRENCY_FROMATTER = new Intl.NumberFormat(undefined, {
-    currency: "AUD",
-    style: "currency",
-})
-
-export function formatCurrency(number){
-    return CURRENCY_FROMATTER.format(number)
-}
--- src/utilities/localStorageFns.jsx
+++
@@ -1,8 +0,0 @@
-export const setItemInStorage = (name, data) => localStorage?.setItem(
-    name, JSON.stringify(data))
-
-export const getItemFromStorage = (name) => localStorage?.getItem(name)
-
-export const getParsedItemFromStorage = (name) => JSON.parse(localStorage?.getItem(name))
-
-export const removeItemFromStorage = (name) => localStorage?.removeItem(name)
